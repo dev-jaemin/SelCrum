e488a1837f6ff5dc7c69a28d9a366b6ba84090c0
Jaemin Kim <jay.kimjaemin@gmail.com>
2021-08-14 03:57:12 +0000

    ProjectAddPage with react-bootstrap

--- frontend/src/pages/ProjectAddPage.js
+++ frontend/src/pages/ProjectAddPage.js
@@ -1,24 +1,40 @@
 import './ProjectAddPage.css';
 
 import { Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Button} from "react-bootstrap";
 
  
 
 function ProjectAddPage() {
   return (
     <div className="ProjectAddPage">
-    	<form action='/' method='post'>
-			<div>
-				<h4>프로젝트 제목</h4>
-				<input type='text' name='name'/>
-				<h4>목표</h4>
-				<input type='text' name='goal'/>
-				<button>추가</button>
-				<h4>기간</h4>
-				<input type='date' name='end_date'/>
-			</div>
-		</form>
+		<Form>
+  			<Form.Group className="mb-3" controlId="formBasicEmail">
+    			<Form.Label size="lg">Project Name</Form.Label>
+    			<Form.Control type="text" placeholder="Enter project name" />
+  			</Form.Group>
+
+  			<Form.Group className="mb-3" controlId="formBasicPassword">
+    			<Form.Label>Goal</Form.Label>
+    			<Form.Control type="text" placeholder="목표" />
+				<Form.Text className="text-muted">
+      				목표 한 가지를 적으시고 등록 버튼을 눌러주세요!
+    			</Form.Text>
+				<Button className="addGoalBtn">등록</Button>
+  			</Form.Group>
+  
+			<Form.Group className="mb-3" controlId="formBasicPassword">
+    			<Form.Label>Deadline</Form.Label>
+    			<Form.Control type="date"/>
+  			</Form.Group>
+			  
+  			<Button variant="primary" type="submit">
+    			프로젝트 추가
+  			</Button>
+		</Form>
     </div>
+	 
   );
 }
 
