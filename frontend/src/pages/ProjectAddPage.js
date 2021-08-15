086cbba6ef8d9c9e533394585a8d1c8726104639
Jaemin Kim <jay.kimjaemin@gmail.com>
2021-08-15 14:07:05 +0000

    form & api post request

--- frontend/src/pages/ProjectAddPage.js
+++ frontend/src/pages/ProjectAddPage.js
@@ -1,32 +1,74 @@
 import './ProjectAddPage.css';
 
+import React, {useState} from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Form, Button} from "react-bootstrap";
 
+//https://darrengwon.tistory.com/337
  
 
 function ProjectAddPage() {
+	const [name, setName] = useState('');
+	const [goal, setGoal] = useState('');
+	const [deadline, setDeadline] = useState('');
+	
+	const nameHandler = (e) => {
+    	e.preventDefault();
+    	setName(e.target.value);
+  	};
+
+    const goalHandler = (e) => {
+    	e.preventDefault();
+    	setGoal(e.target.value);
+  	};
+	
+	const deadlineHandler = (e) => {
+    	e.preventDefault();
+    	setDeadline(e.target.value);
+  	};
+	
+	const submitHandler = (e) => {
+    	e.preventDefault();
+    	// state에 저장한 값을 가져옵니다.
+    	console.log(name);
+    	console.log(goal);
+		console.log(deadline);
+		
+    	let body = {
+      		name: name,
+      		goal: goal,
+			deadline: deadline
+    	};
+
+		/*
+    	axios
+      	.post("http://localhost:5000/api/projects", body)
+      	.then((res) => console.log(res));
+		*/
+  };
+	
   return (
     <div className="ProjectAddPage">
-		<Form>
+		<Form onSubmit={submitHandler}>
   			<Form.Group className="mb-3" controlId="formBasicEmail">
     			<Form.Label size="lg">Project Name</Form.Label>
-    			<Form.Control type="text" placeholder="Enter project name" />
+    			<Form.Control type="text" placeholder="Enter project name" value={name} onChange={nameHandler}/>
   			</Form.Group>
 
-  			<Form.Group className="mb-3" controlId="formBasicPassword">
+  			<Form.Group className="mb-3" controlId="formBasicGoal">
     			<Form.Label>Goal</Form.Label>
-    			<Form.Control type="text" placeholder="목표" />
-				<Form.Text className="text-muted">
+				<Form.Control as="textarea" rows={3} value={goal} onChange={goalHandler}/>
+				{/*<Form.Text className="text-muted">
       				목표 한 가지를 적으시고 등록 버튼을 눌러주세요!
     			</Form.Text>
-				<Button className="addGoalBtn">등록</Button>
+				<Button className="addGoalBtn">등록</Button>*/}
   			</Form.Group>
   
-			<Form.Group className="mb-3" controlId="formBasicPassword">
+			<Form.Group className="mb-3" controlId="formBasicDeadline">
     			<Form.Label>Deadline</Form.Label>
-    			<Form.Control type="date"/>
+    			<Form.Control type="date" value={deadline} onChange={deadlineHandler}/>
   			</Form.Group>
 			  
   			<Button variant="primary" type="submit">
