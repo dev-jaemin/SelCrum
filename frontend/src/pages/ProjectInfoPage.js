03d74c094c43c3b5a8f4a2cd0f44abd8a6149e7d
Jaemin Kim <jay.kimjaemin@gmail.com>
2021-08-22 03:46:56 +0000

    done ProjectInfoPage

--- frontend/src/pages/ProjectInfoPage.js
+++ frontend/src/pages/ProjectInfoPage.js
@@ -4,14 +4,16 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card} from "react-bootstrap";
+import {Card, Button, Nav} from "react-bootstrap";
 import SprintCard from "../components/SprintCard";
 
 //https://darrengwon.tistory.com/337
  
 
-function ProjectInfoPage() {
-	
+function ProjectInfoPage({match}, props) {
+	let projectId = match.params.projectId;
+	let KanbanPageUrl = '/kanban/' + projectId;
+	let SprintPageUrl = '/sprint/' + projectId;
 	
   return (
     <div className="ProjectInfoPage">
@@ -23,7 +25,18 @@ function ProjectInfoPage() {
 		<SprintCard name="1주차" todos="목업 파일 만들기, 개발환경 셋팅"/>
 		<SprintCard name="2주차" todos="목업 파일 만들기, 개발환경 셋팅"/>
 		<SprintCard name="3주차" todos="목업 파일 만들기, 개발환경 셋팅"/>
-		  
+		<SprintCard name="4주차" todos="목업 파일 만들기, 개발환경 셋팅"/>
+		<Button className="addSprintBtn" variant="primary">
+    		+
+  		</Button>
+		  <Nav variant="pills" defaultActiveKey={SprintPageUrl} className="SprintPageNav justify-content-center">
+  			<Nav.Item key='1'>
+    			<Nav.Link href={KanbanPageUrl}>칸반 보드</Nav.Link>
+  			</Nav.Item>
+  			<Nav.Item key='2'>
+    			<Nav.Link href={SprintPageUrl}>스프린트</Nav.Link>
+  			</Nav.Item>
+  		</Nav>
     </div>
 	 
   );
