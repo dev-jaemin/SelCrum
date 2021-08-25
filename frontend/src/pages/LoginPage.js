2f5493a45cf6f4902e1d8bb1cd7f7c1e8e5cc694
Jaemin Kim <jay.kimjaemin@gmail.com>
2021-08-25 09:11:11 +0000

    done loginpage

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import './LoginPage.css';
 
 import { Route } from 'react-router-dom';
@@ -6,25 +7,54 @@ import {Form, Button} from "react-bootstrap";
 
 
 function LoginPage({history}) {
+	const [id, setId] = useState('');
+	const [password, setPassword] = useState('');
+	
+	const idHandler = (e) => {
+    	e.preventDefault();
+    	setId(e.target.value);
+  	};
+
+    const passwordHandler = (e) => {
+    	e.preventDefault();
+    	setPassword(e.target.value);
+  	};
+
+	
+	const submitHandler = (e) => {
+    	e.preventDefault();
+    	// state에 저장한 값을 가져옵니다.
+		
+    	let body = {
+      		id: id,
+      		password: password
+    	};
+
+		/*
+    	axios
+      	.post("http://localhost:5000/api/projects", body)
+      	.then((res) => console.log(res));
+		*/
+  };
 	
   return (
     <div className="LoginPage">
     	<Form>
   			<Form.Group className="mb-3" controlId="formBasicEmail">
     			<Form.Label>ID</Form.Label>
-    			<Form.Control type="text" placeholder="아이디" />
+    			<Form.Control type="text" placeholder="아이디" onChange={idHandler}/>
     			
 				
   			</Form.Group>
 
   			<Form.Group className="mb-3" controlId="formBasicPassword">
     			<Form.Label>Password</Form.Label>
-    			<Form.Control type="password" placeholder="비밀번호" />
+    			<Form.Control type="password" placeholder="비밀번호" onChange={passwordHandler}/>
   			</Form.Group>
   			<Form.Group className="mb-3" controlId="formBasicCheckbox">
     			<Form.Check type="checkbox" label="Check me out" />
   			</Form.Group>
-  			<Button variant="primary" type="submit">
+  			<Button variant="primary" type="submit" onClick={submitHandler}>
     			시작하기
   			</Button>
 		</Form>
